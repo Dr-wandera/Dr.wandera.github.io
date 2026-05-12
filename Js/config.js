cba6aeab40b6c29826ed831519afb90e65c8793e
Dr-wandera <wanderafaustine@gmail.com>
2026-05-12 16:00:50 +0300

    Added mobile view mode

---
+++ Js/config.js
@@ -0,0 +1,14 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyC1o-uyxJoPs3_PtDQ5ZEQ8mGyPGaxQ8yU",
+    authDomain: "industrial-training-6dc37.firebaseapp.com",
+    databaseURL: "https://industrial-training-6dc37-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "industrial-training-6dc37",
+    storageBucket: "industrial-training-6dc37.firebasestorage.app",
+    messagingSenderId: "933608618747",
+    appId: "1:933608618747:web:6c9cc335c33e6271a38e5e",
+    measurementId: "G-P6TV7HN5GE"
+};
+
+firebase.initializeApp(firebaseConfig);
+const auth= firebase.auth()
+console.log("connecting to firebase")
